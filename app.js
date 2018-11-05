ff129a5e47f78cb2172ac6b59e554c8efa3101d2
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-11-05 21:29:23 +0700

    add fork()

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const shell = require("shelljs");
+const { fork } = require("child_process");
 const app = express();
 const port = 6789;
 
 app.get("/", dowork);
 
 async function dowork(req, res) {
-  shell.exec("./script.sh");
-  shell.exec("../sshalert.sh")
-  res.send("Done");
+  if (request.url === "/compute") {
+    const compute = fork(() => {
+      shell.exec("../sshalert.sh");
+      shell.exec("./script.sh");
+    });
+    compute.send("start");
+  } else {
+    res.end("Route not found");
+  }
 }
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
