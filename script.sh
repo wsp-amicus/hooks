0e06c79e4972b819bda1d199b177f05cad087119
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-11-05 20:50:43 +0700

    fix

--- script.sh
+++ script.sh
@@ -5,4 +5,4 @@ npm run build;
 sudo kill $(sudo lsof -t -i :5555);
 sudo kill $(sudo lsof -t -i :5556);
 ../sshalert.sh;
-PORT=5555 NODE_ENV='production' npm run backend:node;
+PORT=5555 NODE_ENV='production' npm run backend:node & exit;
