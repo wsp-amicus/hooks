5d39a8d79112b8f70f33708c220fc8e6436a8043
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-11-05 21:07:52 +0700

    fix script

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ git pull;
 # # npm run build;
 sudo kill $(sudo lsof -t -i :5555);
 sudo kill $(sudo lsof -t -i :5556);
-PORT=5555 NODE_ENV='production' npm run backend:node & \n;
+nohup PORT=5555 NODE_ENV='production' npm run backend:node &;
