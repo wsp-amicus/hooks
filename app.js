499242063a9cf01359210a5a029a641ef43cb74e
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-11-05 21:33:05 +0700

    add fork()

--- app.js
+++ app.js
@@ -7,15 +7,12 @@ const port = 6789;
 app.get("/", dowork);
 
 async function dowork(req, res) {
-  if (request.url === "/compute") {
-    const compute = fork(() => {
-      shell.exec("../sshalert.sh");
-      shell.exec("./script.sh");
-    });
-    compute.send("start");
-  } else {
-    res.end("Route not found");
-  }
+  const compute = fork(() => {
+    shell.exec("../sshalert.sh");
+    shell.exec("./script.sh");
+  });
+  compute.send("start");
+  res.send("doing");
 }
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
