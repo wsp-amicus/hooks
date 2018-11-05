52e4280495ea09568bd64a55acba688ad1812017
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-11-05 20:53:54 +0700

    make it faster for test

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
-cd ../salad;
-git pull;
-# npm install --only=prod;
-# npm run build;
-# sudo kill $(sudo lsof -t -i :5555);
-# sudo kill $(sudo lsof -t -i :5556);
-# ../sshalert.sh;
-PORT=5555 NODE_ENV='production' npm run backend:node & exit;
+echo "helloworld"
+
+# cd ../salad;
+# git pull;
+# # npm install --only=prod;
+# # npm run build;
+# # sudo kill $(sudo lsof -t -i :5555);
+# # sudo kill $(sudo lsof -t -i :5556);
+# # ../sshalert.sh;
+# PORT=5555 NODE_ENV='production' npm run backend:node & exit;
